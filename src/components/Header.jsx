37ec4903b663ea154016000bc49d6596c5d5a162
limazxz <goubulsegue@gmail.com>
2025-04-22 09:29:27 -0300

    ajustando e estilizando header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
 
 const Header = () => {
   return (
-    <div className="w-100 bg-black text-success">Header</div>
-  )
-}
+    <header className="w-100 navbar navbar-dark bg-dark gap-5 w-50 justify-content-between">
+      <div id="logo-pesq" className="d-flex">
+        <div id="logo" role="button" className="d-flex">
+          <i className="bi bi-controller fs-1 text-light me-2"></i>
+          <span className="navbar-brand fw-bold fs-3">DevSteam</span>
+        </div>
+        <input
+          type="text"
+          placeholder="Buscar"
+          className="bg-secondary-subtle d-none d-md-block border-0 rounded-3 search-input px-4 my-2"
+        />
+      </div>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
