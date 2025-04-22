6fb8bffa25a0803ef55c6dfbffe74e254e3dfba2
limazxz <goubulsegue@gmail.com>
2025-04-22 15:26:47 -0300

    realizando ajustes do promoCard e promotion

--- src/components/Promotion.jsx
+++ src/components/Promotion.jsx
@@ -2,17 +2,93 @@ import React from "react";
 import PromoCard from "./PromoCard";
 
 const Promotion = () => {
+  const games = [
+    {
+      id: 1,
+      titulo: "Counter-Strike 2",
+      preco: 0.0,
+      desconto: 0,
+      imagem:
+        "https://cdn.cloudflare.steamstatic.com/steam/apps/730/header.jpg",
+    },
+    {
+      id: 2,
+      titulo: "Cyberpunk 2077",
+      preco: 129.99,
+      desconto: 20, // 20% off
+      imagem:
+        "https://cdn.cloudflare.steamstatic.com/steam/apps/1091500/header.jpg",
+    },
+    {
+      id: 3,
+      titulo: "Elden Ring",
+      preco: 249.9,
+      desconto: 35, // 35% off
+      imagem:
+        "https://cdn.cloudflare.steamstatic.com/steam/apps/1245620/header.jpg",
+    },
+    {
+      id: 4,
+      titulo: "Red Dead Redemption 2",
+      preco: 199.9,
+      desconto: 40, // 40% off
+      imagem:
+        "https://cdn.cloudflare.steamstatic.com/steam/apps/1174180/header.jpg",
+    },
+    {
+      id: 1,
+      titulo: "Counter-Strike 2",
+      preco: 0.0,
+      desconto: 0,
+      imagem:
+        "https://cdn.cloudflare.steamstatic.com/steam/apps/730/header.jpg",
+    },
+    {
+      id: 2,
+      titulo: "Cyberpunk 2077",
+      preco: 129.99,
+      desconto: 20, // 20% off
+      imagem:
+        "https://cdn.cloudflare.steamstatic.com/steam/apps/1091500/header.jpg",
+    },
+    {
+      id: 3,
+      titulo: "Elden Ring",
+      preco: 249.9,
+      desconto: 35, // 35% off
+      imagem:
+        "https://cdn.cloudflare.steamstatic.com/steam/apps/1245620/header.jpg",
+    },
+    {
+      id: 4,
+      titulo: "Red Dead Redemption 2",
+      preco: 199.9,
+      desconto: 40, // 40% off
+      imagem:
+        "https://cdn.cloudflare.steamstatic.com/steam/apps/1174180/header.jpg",
+    },
+  ];
+
   return (
-    <div id="promotion">
-      <h2>Promoções</h2>
+    <div id="promotion" className="container w-75 my-5">
+      <h2 className="text-uppercase">Promoções</h2>
       <div
         id="itensPromo"
-        className="d-flex flex-wrap justify-content-around gap-4"
+        className="d-flex flex-wrap gap-4 justify-content-between"
       >
-        {/* inserir os card de promoção */}
-        <PromoCard />
-        <PromoCard />
-        <PromoCard />
+        {/* mapeando um array com react */}
+        {games
+        .slice(0, 4) // Limita a exibição a apenas 3 jogos
+        .filter((jogo) => jogo.desconto > 0) // Filtra os jogos com desconto
+        .map((jogo) => (
+          <PromoCard
+            key={jogo.id}
+            titulo={jogo.titulo}
+            preco={jogo.preco.toFixed(2)}
+            desconto={jogo.desconto}
+            imagem={jogo.imagem}
+          />
+        ))}
       </div>
     </div>
   );
