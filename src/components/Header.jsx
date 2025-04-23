2a92d56137340f28810b48b28c0b99429e7ac359
limazxz <goubulsegue@gmail.com>
2025-04-23 16:49:34 -0300

    implementa componente CarrinhoOffCanvas e lógica de checkout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,48 +1,129 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
+import { useNavigate } from "react-router";
 
-const Header = (props) => {
-  const [usuario, setUsuario] = useState(null);
+const CarrinhoOffCanvas = (props) => {
+  const navigate = useNavigate();
 
-  useEffect(() => {
-    const salvaUsaurio = localStorage.getItem("devlogin");
-    salvaUsaurio && setUsuario(JSON.parse(salvaUsaurio));
-  });
+  const total = props.carrinhoItem.reduce(
+    (acc, item) =>
+      acc + (item.preco - (item.preco * item.desconto) / 100) * item.quantidade,
+    0
+  );
+
+  const goToCheckout = () => {
+    navigate("/checkout");
+  };
 
   return (
-    <header className="pt-4 w-100 navbar navbar-dark bg-dark justify-content-around align-items-center">
-      <div id="info" className="d-flex gap-5 w-50 justify-content-between">
-        <div id="logo" role="button" className="d-flex align-items-center me-5">
-          <i className="bi bi-controller fs-1 text-light me-3"></i>
-          <span className="navbar-brand fw-bold fs-3">DevSteam</span>
-        </div>
-
-        <input
-          type="text"
-          className="w-100 d-none d-md-block border-0 rounded-1 buscar px-4 my-2 ms-5"
-          placeholder="Buscar..."
-        />
-      </div>
-
+    <div
+      id="carrinhoOffCanvas"
+      className="offcanvas offcanvas-end"
+      style={{ background: "#364A63" }}
+    >
       <div
-        id="carrinho"
-        className="position-relative d-flex align-items-center gap-3"
+        className="offcanvas-header text-light"
+        style={{ background: "#1b2838" }}
       >
-        {usuario && <span>Olá, {usuario.nome.split(" ")[0]}!</span>}
+        <h5 className="offcanvas-title">
+          {" "}
+          <i className="bi bi-cart4 me-2 fs-4"></i>Carrinho
+        </h5>
         <i
           role="button"
-          data-bs-toggle="offcanvas"
-          data-bs-target="#carrinhoOffCanvas"
-          className="bi bi-cart4 text-light fs-2"
+          className="ms-auto fs-2 p-0 m-0 bi bi-x"
+          data-bs-dismiss="offcanvas"
         ></i>
+      </div>
 
-        {props.contadorJogos >= 0 && (
-          <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
-            {props.contadorJogos}
-          </span>
+      <div className="offcanvas-body">
+        {props.carrinhoItem.length === 0 ? (
+          <p className="text-center text-light">Seu carrinho está vazio.</p>
+        ) : (
+          <>
+            <ul className="list-group list-group-flush">
+              {props.carrinhoItem.map((item) => (
+                <li
+                  key={item.id}
+                  className="px-4 py-3 shadow mt-3 d-flex gap-3 rounded "
+                  style={{ background: "#d9d9d9" }}
+                >
+                  <img
+                    className="object-fit-cover rounded-2"
+                    src={item.imagem}
+                    alt={item.titulo}
+                    width={60}
+                    height={80}
+                  />
+                  <div className="w-100">
+                    <div className="d-flex justify-content-between">
+                      <h6 className="fw-bold p-1">{item.titulo}</h6>
+                      <i
+                        role="button"
+                        className="bi bi-trash fs-5 text-danger"
+                        onClick={() => props.onRemoveCarrinho(item)}
+                      ></i>
+                    </div>
+
+                    <div className="d-flex justify-content-between">
+                      <div className="border border-dark-subtle border-1 d-flex align-items-center rounded-4 gap-2">
+                        <button
+                          className="btn border-0"
+                          disabled={item.quantidade === 1}
+                          onClick={() =>
+                            props.onUpdateCarrinho(item, item.quantidade - 1)
+                          }
+                        >
+                          -
+                        </button>
+                        <span>{item.quantidade}</span>
+                        <button
+                          className="btn border-0"
+                          onClick={() =>
+                            props.onUpdateCarrinho(item, item.quantidade + 1)
+                          }
+                        >
+                          +
+                        </button>
+                      </div>
+
+                      <div className="d-flex flex-column align-items-end">
+                        <span className="text-decoration-line-through small">
+                          R$
+                          {item.preco.toFixed(2)}
+                        </span>
+                        <span className="fw-bolder">
+                          R$
+                          {(
+                            item.preco -
+                            (item.preco * item.desconto) / 100
+                          ).toFixed(2)}
+                        </span>
+                      </div>
+                    </div>
+                  </div>
+                </li>
+              ))}
+            </ul>
+
+            <hr className="text-white" />
+            <div className="d-flex justify-content-between text-light fs-4">
+              <strong>Total:</strong>
+              <strong>R$ {total.toFixed(2)}</strong>
+            </div>
+            <button
+              id="addCarrinho"
+              className="btn btn-success desconto border-0 w-100 mt-2 fs-5"
+              data-bs-toggle="offcanvas"
+              data-bs-target="#carrinhoOffCanvas"
+              onClick={goToCheckout}
+            >
+              Finalizar Compra
+            </button>
+          </>
         )}
       </div>
-    </header>
+    </div>
   );
 };
 
-export default Header;
+export default CarrinhoOffCanvas;
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 import App from "./App.jsx";
-import { BrowserRouter, Routes, Route } from "react-router";
 import Login from "./pages/Login.jsx";
 import Checkout from "./pages/Checkout.jsx";
 
